a4965bfd882c2933fce12c96da9a3e8c9fac5dfb
vanisri1 <innijack@gmail.com>
2019-11-12 18:59:33 +0530

    newly updated vani

--- src/org/test/Main.java
+++ src/org/test/Main.java
@@ -3,5 +3,13 @@ package org.test;
 public class Main {
 public static void main(String[] args) {
 System.out.println("vanisri");
+System.out.println("vanisri");
+System.out.println("vanisri");
+System.out.println("vanisri");
+System.out.println("vanisri");
+System.out.println("vanisri");
+System.out.println("vanisri");
+System.out.println("vanisri");
+
 }
 }
