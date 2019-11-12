0a4d969772331e1f371994154f38d07a4815591b
vanisri1 <innijack@gmail.com>
2019-11-12 19:11:01 +0530

    conflits jack

--- src/org/test/Main.java
+++ src/org/test/Main.java
@@ -2,11 +2,14 @@ package org.test;
 
 public class Main {
 public static void main(String[] args) {
-System.out.println("123434");
-System.out.println("ewrftghj");
-System.out.println("sgfsdgfs");
-System.out.println("vcxv");
-System.out.println("bfbd");
-
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+	System.out.println("vanisri");
+System.out.println("dai jack");
 }
 }
