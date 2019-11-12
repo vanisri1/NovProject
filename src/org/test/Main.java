1c884b4933217a6c9df1fab7705857e460cbb84f
vanisri1 <innijack@gmail.com>
2019-11-12 19:06:04 +0530

    conflits jack

--- src/org/test/Main.java
+++ src/org/test/Main.java
@@ -2,6 +2,11 @@ package org.test;
 
 public class Main {
 public static void main(String[] args) {
-System.out.println("vanisri");
+System.out.println("123434");
+System.out.println("ewrftghj");
+System.out.println("sgfsdgfs");
+System.out.println("vcxv");
+System.out.println("bfbd");
+
 }
 }
